c83645d5f6b3077e77870c64abee2ec493f9d189
Jamie <jamiekaram16@gmail.com>
2020-03-04 13:06:55 -0500

    search actor name with picture

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
 import _ from 'lodash'
@@ -10,24 +9,21 @@ function App() {
   const [text, setText] = useState('');
   const [results, setResults] = useState([]);
 
-  function handleSetText(text) {
-    _.debounce(setText(text), 2000)
+  const search = (text) => {
+    setText(text)
+    axios.get(`https://api.themoviedb.org/3/search/person?api_key=a405299a0939a3be4b4586ea1f4363e3&language=en-US&query=${text}&page=1&include_adult=false`)
+      .then(function (response) {
+        setResults(response.data.results.filter(actor => actor.known_for_department === 'Acting'))
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
   }
 
-
-  axios.get(`https://api.themoviedb.org/3/search/person?api_key=a405299a0939a3be4b4586ea1f4363e3&language=en-US&query=${text}&page=1&include_adult=false`)
-    .then(function (response) {
-     console.log(response.data.results)
-      
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
-
-
+  console.log(results)
 
   return (
-   
+
     <div className="App">
       <header className="App-header">
 
@@ -36,10 +32,23 @@ function App() {
           maxLength="50"
           value={text}
           placeholder="Enter your parlay Name"
-          onChange={v => handleSetText(v.target.value)}
+          onChange={v =>
+            search(v.target.value)
+          }
         />
-        {text}
-     
+
+
+        {
+          results.slice(0, 5).map(actor => {
+            return (
+              <>
+                <p>{actor.name}</p>
+                <img src={`https://image.tmdb.org/t/p/original${actor.profile_path}`} height='100px' width="auto" />
+              </>
+            )
+          })
+        }
+
 
       </header>
     </div>
